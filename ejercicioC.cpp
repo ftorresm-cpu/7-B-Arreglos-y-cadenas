15555b173920a93f4592249209432781b1c59063
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-21 17:22:35 -0500

    EjercicioC: Primer intento usando un arreglo de contadores

---
+++ ejercicioC.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int P, n;
+    cin >> P >> n;
+    
+    int votos[105];
+    for (int i = 1; i <= P; i++) {
+        votos[i] = 0;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        int profesorVotado;
+        cin >> profesorVotado;
+        
+        votos[profesorVotado] = votos[profesorVotado] + 1;
+    }
+    
+    for (int i = 1; i <= P; i++) {
+        cout << i << "-" << votos[i] << endl;
+    }
+    
+    return 0;
+}
