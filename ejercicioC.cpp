079b9c05858c9d1a161ccefa27c5cc98abefc22d
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-21 17:23:45 -0500

    EjercicioC: Segunda version inicializando el arreglo en cero

--- ejercicioC.cpp
+++ ejercicioC.cpp
@@ -5,15 +5,11 @@ int main() {
     int P, n;
     cin >> P >> n;
     
-    int votos[105];
-    for (int i = 1; i <= P; i++) {
-        votos[i] = 0;
-    }
+    int votos[105] = {0}; 
     
     for (int i = 0; i < n; i++) {
         int profesorVotado;
         cin >> profesorVotado;
-        
         votos[profesorVotado] = votos[profesorVotado] + 1;
     }
     
