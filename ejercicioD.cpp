3da8f973981681df6409cc512126f2b5d9dc8f51
ftorresm <ftorresm@unjbg.edu.pe>
2026-06-22 23:45:26 -0500

    EjercicioD: Primer intento usando un arreglo para contar canicas

---
+++ ejercicioD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, C;
+    cin >> N >> C;
+    
+    int vasos[105];
+    for (int i = 1; i <= N; i++) {
+        vasos[i] = 0;
+    }
+    
+    for (int i = 0; i < C; i++) {
+        int vasoLanzado;
+        cin >> vasoLanzado;
+        
+        vasos[vasoLanzado] = vasos[vasoLanzado] + 1;
+    }
+    
+    for (int i = 1; i <= N; i++) {
+        cout << vasos[i] << endl;
+    }
+    
+    return 0;
+}
